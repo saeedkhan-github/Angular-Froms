6d166233c83803e0805133dd8f28954764df1d7b
SaeedUllah <saeedullah321@gmail.com>
2019-02-26 23:22:49 +0500

    commit

---
+++ src/app/name-editor/name-editor.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-name-editor',
+  templateUrl: './name-editor.component.html',
+  styleUrls: ['./name-editor.component.css']
+})
+export class NameEditorComponent {
+
+    name = new FormControl('');
+
+}
